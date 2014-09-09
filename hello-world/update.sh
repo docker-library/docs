1ec235ca00bfa4b25612d046cf3fa6c5daad8d35
Tianon Gravi <admwiggin@gmail.com>
2014-09-09 15:36:42 -0600

    Add simple script to auto-update the hello-world description

---
+++ hello-world/update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+docker pull hello-world &> /dev/null
+
+exec > "$(dirname "$(readlink -f "$BASH_SOURCE")")/README-content.md"
+
+echo '# Example output'
+echo
+
+{
+	echo '$ docker run hello-world'
+	docker run --rm hello-world
+	echo
+	echo '$ docker images hello-world'
+	docker images hello-world | awk -F'  +' '{ print $1"\t"$2"\t"$3"\t"$5 }' | column -t -s$'\t'
+} | sed 's/^/    /'
