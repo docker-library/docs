7c10d1092acf92b1bbcb0e217b34c538b4d2ee44
Tianon Gravi <admwiggin@gmail.com>
2015-10-22 16:52:58 -0700

    Reflow stub readme text and add Travis badge

--- generate-repo-stub-readme.sh
+++ generate-repo-stub-readme.sh
@@ -22,10 +22,9 @@ hubPage="https://registry.hub.docker.com/_/$repo/"
 cat <<EOREADME
 # About this Repo
 
-This is the Git repo of the official Docker image for [$repo]($hubPage). See the
-Hub page for the full readme on how to use the Docker image and for information
-regarding contributing and issues.
+This is the Git repo of the official Docker image for [$repo]($hubPage). See the Hub page for the full readme on how to use the Docker image and for information regarding contributing and issues.
 
-The full readme is generated over in [docker-library/docs]($gitRepo),
-specificially in [docker-library/docs/$repo]($gitRepo/tree/master/$repo).
+The full readme is generated over in [docker-library/docs]($gitRepo), specificially in [docker-library/docs/$repo]($gitRepo/tree/master/$repo).
+
+[![Travis CI](https://img.shields.io/travis/docker-library/$repo/master.svg)](https://travis-ci.org/docker-library/$repo/branches)
 EOREADME
