4c1d14a1ee356b2ac7e430a2cd87047d2a440d65
Tianon Gravi <admwiggin@gmail.com>
2017-06-30 23:35:54 -0700

    Apply the changes from #840 to "hello-world/update.sh" (so they propagate properly)
    See commit 63052cadffe8b17f0f362e4845504842be53a0ee.

--- hello-world/update.sh
+++ hello-world/update.sh
@@ -23,5 +23,5 @@ echo
 cat <<'EOF'
 # How is this image created?
 
-This image is a prime example of using the [`scratch`](https://registry.hub.docker.com/_/scratch/) image effectively. See [`hello.asm`](%%GITHUB-REPO%%/blob/master/hello.asm) in %%GITHUB-REPO%% for the source code of the `hello` binary included in this image.
+This image is a prime example of using the [`scratch`](https://hub.docker.com/_/scratch/) image effectively. See [`hello.c`](%%GITHUB-REPO%%/blob/master/hello.c) in %%GITHUB-REPO%% for the source code of the `hello` binary included in this image.
 EOF
