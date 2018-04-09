341f6017cbfc653c0754be42abe4b2054623d060
Tianon Gravi <admwiggin@gmail.com>
2018-04-09 13:52:19 -0700

    Adjust repo stub wording

--- generate-repo-stub-readme.sh
+++ generate-repo-stub-readme.sh
@@ -17,7 +17,7 @@ EOUSAGE
 fi
 
 gitRepo='https://github.com/docker-library/docs'
-hubPage="https://registry.hub.docker.com/_/$repo/"
+hubPage="https://hub.docker.com/_/$repo/"
 
 canonicalRepo="https://github.com/docker-library/$repo"
 if [ -s "$repo/github-repo" ]; then
@@ -36,11 +36,11 @@ fi
 cat <<EOREADME
 # About this Repo
 
-This is the Git repo of the Docker [official image](https://docs.docker.com/docker-hub/official_repos/) for [$repo]($hubPage). See [the Docker Hub page]($hubPage) for the full readme on how to use this Docker image and for information regarding contributing and issues.
+This is the Git repo of the [Docker "Official Image"](https://docs.docker.com/docker-hub/official_repos/) for [$repo]($hubPage) (which does not necessarily mean it is "official" according to $repo upstream). See [the Docker Hub page]($hubPage) for the full readme on how to use this Docker image and for information regarding contributing and issues.
 
-The full readme is generated over in [docker-library/docs]($gitRepo), specifically in [docker-library/docs/$repo]($gitRepo/tree/master/$repo).
+The [full description from Docker Hub]($hubPage) is generated over in [docker-library/docs]($gitRepo), specifically in [docker-library/docs/$repo]($gitRepo/tree/master/$repo).
 
-See a change merged here that doesn't show up on the Docker Hub yet? Check [the "library/$repo" manifest file in the docker-library/official-images repo](https://github.com/docker-library/official-images/blob/master/library/$repo), especially [PRs with the "library/$repo" label on that repo](https://github.com/docker-library/official-images/labels/library%2F$repo). For more information about the official images process, see the [docker-library/official-images readme](https://github.com/docker-library/official-images/blob/master/README.md).
+See a change merged here that doesn't show up on Docker Hub yet? Check [the "library/$repo" manifest file in the docker-library/official-images repo](https://github.com/docker-library/official-images/blob/master/library/$repo), especially [PRs with the "library/$repo" label on that repo](https://github.com/docker-library/official-images/labels/library%2F$repo). For more information about the official images process, see the [docker-library/official-images readme](https://github.com/docker-library/official-images/blob/master/README.md).
 EOREADME
 
 badges=()
